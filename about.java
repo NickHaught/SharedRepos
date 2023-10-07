4d8cbb5bef5dd422d16eeac7e3bc0af04beedcc0
NickHaught <nicholas.haught@outlook.com>
2023-10-07 18:38:21 -0400

    first commit

---
+++ about.java
@@ -0,0 +1,10 @@
+package about;
+
+
+public class about {
+	public static void main(String[] args) {
+		System.out.println("Name: Nick");
+		System.out.println("Major: Computer Science & Applied Physics");
+		System.out.println("First Job: Software developer in a statup");
+	}
+}
