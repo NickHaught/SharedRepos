36f71c804ca96c3a35ad354ec1a79daedbdd94d1
KDBlough <147455164+KDBlough@users.noreply.github.com>
2023-10-10 15:28:43 -0400

    Create about_kyria.java
    A description of Kyria

---
+++ about_kyria.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Name: Kyria");
+    System.out.println("Major: Computer Science");
+    System.out.println("First Job: Web Developer/Programmer for a company.");
+  }
+}
