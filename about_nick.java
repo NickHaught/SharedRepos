6e6771f8dddfa9191fdb2c5665486c3bd027ceec
bvancamp <bob.vancamp@marietta.edu>
2023-10-10 15:24:13 -0400

    Update about_nick.java
    Added the "r"

--- about_nick.java
+++ about_nick.java
@@ -5,6 +5,6 @@ public class about {
 	public static void main(String[] args) {
 		System.out.println("Name: Nick");
 		System.out.println("Major: Computer Science & Applied Physics");
-		System.out.println("First Job: Software developer in a statup");
+		System.out.println("First Job: Software developer in a startup");
 	}
 }
