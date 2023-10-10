0d6cd4d7075a53910f879d503585c535371f4809
SahmiW <147407544+SahmiW@users.noreply.github.com>
2023-10-10 15:28:53 -0400

    Create Aboutsahmi.java

---
+++ Aboutsahmi.java
@@ -0,0 +1,13 @@
+public class MyInformation {
+    public static void main(String[] args) {
+        // Declare and initialize variables with your information
+        String name = "Sahmi";
+        String major = "Information Systems";
+        String firstJobLocation = "I dont know yet, Somewhere in the Help desk field";
+
+        // Display the information
+        System.out.println("Name: " + name);
+        System.out.println("Major: " + major);
+        System.out.println("Expected First Job Location: " + firstJobLocation);
+    }
+}
