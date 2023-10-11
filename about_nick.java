292435e3e380d906d8dbc48264c6974587bbf53e
NickHaught <146685642+NickHaught@users.noreply.github.com>
2023-10-11 10:18:05 -0400

    Update about_nick.java

--- about_nick.java
+++ about_nick.java
@@ -1,6 +1,7 @@
 
 public class about_nick {
 	public static void main(String[] args) {
+		System.out.println("Bio: My name is Nick Haught, I am a computer science and applied physics student. I have strong analytical skills that enable me to break down tasks into manageable steps. When working on coding projects, I go above and beyond in learning coding techniques and add additional features. Additionally, I excel in teamwork communication, making me an effective collaborator on group projects. ");
 		System.out.println("Name: Nick");
 		System.out.println("Major: Computer Science & Applied Physics");
 		System.out.println("First Job: Software developer in a startup");
