d43275620fdba7ea328453e608b0e1961cea4c52
NickHaught <146685642+NickHaught@users.noreply.github.com>
2023-10-11 10:16:13 -0400

    Update about_nick.java

--- about_nick.java
+++ about_nick.java
@@ -1,7 +1,5 @@
-package about;
 
-
-public class about {
+public class about_nick {
 	public static void main(String[] args) {
 		System.out.println("Name: Nick");
 		System.out.println("Major: Computer Science & Applied Physics");
